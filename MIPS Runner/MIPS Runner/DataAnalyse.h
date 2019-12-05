c6d3289e96efda65ddd744c18cb3f8de0eff518c
Minh Huy <huy.cse.9@gmail.com>
2019-12-05 21:52:04 +0700

    fix reference link

--- MIPS Runner/MIPS Runner/DataAnalyse.h
+++ MIPS Runner/MIPS Runner/DataAnalyse.h
@@ -1,14 +1,22 @@
 #pragma once
 #include  <iostream>
 #include "LabelManager.h"
+#include "MemoryManager.h"
 
 class DataAnalyse
 {
 private:
+	void* variablePtr;
 	char** tokenData;
 	int tokenListSize;
-	char** parseDataToToken(const char* line);
+	char** parseDataToToken(const char* );
+	void (*function) (const char**,int);
+	static void* currentPtr;
 public:
-	DataAnalyse(const char* line);
+	static int toInt(const char*);
+	DataAnalyse(const char*);
+	void processor(const char**);
+	static void word(const char**,int);
+	static void byte(const char**, int);
 };
 
